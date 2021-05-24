9654998de5e129f8f0be60f6c18443bc44a378e7
Mauricio Jost <mauriciojost@gmail.com>
2021-05-24 18:50:35 +0200

    Script for rust build (xtensa target)

--- build_rust.sh
+++ build_rust.sh
@@ -4,11 +4,10 @@ set -u
 set -x 
 set -e
 
-my_build_root=.
+my_build_root=rust
 
 cd "$my_build_root"
 
-git clone git@github.com:MabezDev/rust-xtensa.git
 cd rust-xtensa
 
 mkdir "$my_build_root/rust_build"
@@ -19,6 +18,8 @@ mkdir "$my_build_root/rust_build"
 # seems to clone something. before help is invoked.
 python ./x.py --help
 
-# Take a while
+# takes a while
 python ./x.py build
 python ./x.py install
+
+# done
