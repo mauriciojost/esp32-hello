d59381b44b3b5460cb644260911808e2f24e7881
Mauricio Jost <mauriciojost@gmail.com>
2021-05-25 01:34:46 +0200

    Script

--- build_rust.sh
+++ build_rust.sh
@@ -6,20 +6,26 @@ set -e
 
 my_build_root=$(dirname $(readlink -e $0))/rust
 # rust-xtensa cannot be a submodule of this project, or its bootstrap fails
-git clone git@github.com:mauriciojost/rust-xtensa.git
-cd rust-xtensa
-git fetch origin esp:esp # safe branch
-git checkout esp
-git submodule update --init --recursive
+if [ ! -e rust-xtensa ]
+then
+    echo "### Not built rust, building..."
+    git clone git@github.com:mauriciojost/rust-xtensa.git
+    cd rust-xtensa
+    git fetch origin esp:esp # safe branch
+    git checkout esp
+    git submodule update --init --recursive
 
-mkdir -p "$my_build_root/rust_build"
-echo "### Configuring..."
-./configure --experimental-targets=Xtensa --llvm-root="$my_build_root/llvm_build" --prefix="$my_build_root/rust_build"
+    mkdir -p "$my_build_root/rust_build"
+    echo "### Configuring..."
+    ./configure --experimental-targets=Xtensa --llvm-root="$my_build_root/llvm_build" --prefix="$my_build_root/rust_build"
 
-# python 2 is required.
+    # python 2 is required.
 
-echo "### Building (may take a while)..."
-python ./x.py build --stage 2
-echo "### Installing..."
-python ./x.py install
+    echo "### Building (may take a while)..."
+    python ./x.py build --stage 2
+    echo "### Installing..."
+    python ./x.py install
+else
+    echo "### Already built rust, skipping..."
+fi
 echo "### Done"
