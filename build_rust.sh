2ed156fd6d82805f73c2f13cb15b44a1a169344a
Mauricio Jost <mauriciojost@gmail.com>
2021-05-24 23:54:33 +0200

    Version that should work (to validate)

--- build_rust.sh
+++ build_rust.sh
@@ -4,23 +4,19 @@ set -u
 set -x 
 set -e
 
-my_build_root=rust
-
-cd "$my_build_root"
+my_build_root=$(dirname $(readlink -e $0))/rust
 
 cd rust-xtensa
 git submodule update --init --recursive
 
-mkdir "$my_build_root/rust_build"
-./configure --llvm-root="$my_build_root/llvm_build" --prefix="$my_build_root/rust_build"
+mkdir -p "$my_build_root/rust_build"
+echo "### Configuring..."
+./configure --experimental-targets=Xtensa --llvm-root="$my_build_root/llvm_build" --prefix="$my_build_root/rust_build"
 
 # python 2 is required.
 
-# seems to clone something. before help is invoked.
-python ./x.py --help
-
-# takes a while
-python ./x.py build
+echo "### Building (may take a while)..."
+python ./x.py build --stage 2
+echo "### Installing..."
 python ./x.py install
-
-# done
+echo "### Done"
--- setenv.sh
+++ setenv.sh
@@ -9,7 +9,7 @@ echo "### Download toolchain for ESP if not yet downloaded..."
 gcc_xtensa_version=xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0
 gcc_xtensa_dir=/tmp/gcc-$gcc_xtensa_version
 gcc_xtensa_subdir=xtensa-esp32-elf
-custom_rustc=$(dirname $(readlink -e $0))/rust-xtensa
+custom_rustc=$(dirname $(readlink -e $0))/rust
 
 if [ ! -e $gcc_xtensa_dir/$gcc_xtensa_subdir/bin ]
 then
