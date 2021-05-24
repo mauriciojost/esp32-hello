6386beaa73aa56b1d5275353126a869212118ded
Mauricio Jost <mauriciojost@gmail.com>
2021-05-25 01:33:07 +0200

    Script

--- build_rust.sh
+++ build_rust.sh
@@ -5,8 +5,11 @@ set -x
 set -e
 
 my_build_root=$(dirname $(readlink -e $0))/rust
-
+# rust-xtensa cannot be a submodule of this project, or its bootstrap fails
+git clone git@github.com:mauriciojost/rust-xtensa.git
 cd rust-xtensa
+git fetch origin esp:esp # safe branch
+git checkout esp
 git submodule update --init --recursive
 
 mkdir -p "$my_build_root/rust_build"
